1a67b35b6f58edbe53b12db166d461bc0c5c17f0
MAGA1314 <garciaagudelomiguelangel@gmail.com>
2024-02-29 19:23:16 -0500

    Initial commit

---
+++ app/src/main/java/com/maga/actividad_calculadora_entrega_moviles/Pantallados.java
@@ -0,0 +1,20 @@
+package com.maga.actividad_calculadora_entrega_moviles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pantallados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantallados);
+    }
+
+
+
+
+
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Actividad_Calculadora_Entrega_Moviles"
+include(":app")
+ 
